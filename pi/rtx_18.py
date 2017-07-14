f1f7b86ad846cc1a6d81accb8df62e1a98e43878
InfyxPro <chdominik0@gmail.com>
2017-07-14 17:25:58 -0500

    Just fixed a small bug

--- pi/rtx_18.py
+++ pi/rtx_18.py
@@ -4,8 +4,8 @@ import serial
 # Program starts!
 class seri(object):
 
-	def __init__(self): # To make anything global to all functions
-		self.ser = serial.Serial('/dev/ttyAMA0', baudrate=9600,
+	def __init__(self): # To make anything global to all functions / Also, for usb connection, use '/dev/ttyUSB1' instead of /dev/ttyAMA0 which is for rx and tx pins! Try:  /dev/ttyS1 or  /dev/ttyS2
+		self.ser = serial.Serial('/dev/ttyUSB1', baudrate=9600,
                     parity=serial.PARITY_NONE,
                     stopbits=serial.STOPBITS_ONE,
                     bytesize=serial.EIGHTBITS
@@ -29,11 +29,11 @@ class seri(object):
 
 if __name__ == "__main__":
 	i = seri()
-	while True:
 		try:
-			user = input('What would you like to send?')
-			i.write(user)
-			i.read()
+            while True:
+                user = input('What would you like to send?')
+                i.write(user)
+                i.read()
 			
 		except KeyboardInterrupt: print ("Exiting Program")
 		
