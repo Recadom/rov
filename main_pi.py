6cca6cdc7b562eea8d139e04dcde3feb4f28de52
Martin Juskelis <juskelism@gmail.com>
2017-09-14 17:47:33 -0500

    fixed spaces

--- main_pi.py
+++ main_pi.py
@@ -18,15 +18,15 @@ joystick.init()
 print (joystick.get_name)
 
 def writeNumber(value):
-        value = int(value)
-	bus.write_byte(address, value)
-	# bus.write_byte_data(address, 0, value)
-	return -1
+    value = int(value)
+    bus.write_byte(address, value)
+    # bus.write_byte_data(address, 0, value)
+    return -1
 
 def readNumber():
-	number = bus.read_byte(address)
-	number = bus.read_byte_data(address, 1)
-	return number
+    number = bus.read_byte(address)
+    number = bus.read_byte_data(address, 1)
+    return number
 ##Setup
 end = True
 axis_forward_back = 1
@@ -35,71 +35,38 @@ axis_up_down = 2
 axis_twist = 3
 t_wait = 0.005
 while True:
-        try:
-                
-                pygame.event.pump()
-                
-                forwLeft = joystick.get_axis(axis_forward_back) * 63 + 64
-                writeNumber(forwLeft)
-                         
-                time.sleep(t_wait)
+    try:
 
-                forwRight = joystick.get_axis(axis_forward_back) * 63 + 64
-                writeNumber(forwRight)
-                time.sleep(t_wait)
-                
-                vertLeft = joystick.get_axis(axis_up_down) * 63 + 64
-                writeNumber(vertLeft)
-                time.sleep(t_wait)
-		
-                vertRight = joystick.get_axis(axis_up_down) * 63 + 64
-                writeNumber(vertRight)
-                time.sleep(t_wait)
+        pygame.event.pump()
 
-                writeNumber(0)
-                time.sleep(t_wait)
+        forwLeft = joystick.get_axis(axis_forward_back) * 63 + 64
+        writeNumber(forwLeft)
 
-                #print(str(int(forwLeft)) + "\t" + str(int(forwLeft)) + "\t" + str(int(vertLeft)) + "\t" + str(int(vertRight)))
+        time.sleep(t_wait)
 
-                
-                if end == True:
-                        end = False
-                        print('Working!')
+        forwRight = joystick.get_axis(axis_forward_back) * 63 + 64
+        writeNumber(forwRight)
+        time.sleep(t_wait)
 
-        except IOError as err:
-                end = True
-                time.sleep(0.5)
-                print (err)
-        
+        vertLeft = joystick.get_axis(axis_up_down) * 63 + 64
+        writeNumber(vertLeft)
+        time.sleep(t_wait)
 
-        except KeyboardInterrupt: pass
+        vertRight = joystick.get_axis(axis_up_down) * 63 + 64
+        writeNumber(vertRight)
+        time.sleep(t_wait)
+
+        writeNumber(0)
+        time.sleep(t_wait)
+
+        #print(str(int(forwLeft)) + "\t" + str(int(forwLeft)) + "\t" + str(int(vertLeft)) + "\t" + str(int(vertRight)))
 
 
+        if end == True:
+            end = False
+            print('Working!')
 
-
-                
-		
-"""
-                forwLeft = joystick.get_axis(axis_forward_back) * 63 + 64
-
-                writeNumber(forwLeft)
-                time.sleep(0.01)
-                
-                writeNumber(0)
-                time.sleep(0.01)
-
-                forwRight = joystick.get_axis(axis_forward_back) * 63 + 64
-                writeNumber(forwRight)
-                time.sleep(0.01)
-
-                vertLeft = joystick.get_axis(axis_up_down) * 63 + 64
-                writeNumber(vertLeft)
-                time.sleep(0.01)
-		
-                vertRight = joystick.get_axis(axis_up_down) * 63 + 64
-                writeNumber(vertRight)
-                time.sleep(0.01)
-
-                writeNumber(0)
-                time.sleep(0.01)
-"""
+    except IOError as err:
+        end = True
+        time.sleep(0.5)
+        print (err)
