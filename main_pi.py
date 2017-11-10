2060f8cf47dbbc494c31526cad9f314db40f3005
Arduino <chdominik0@gmail.com>
2017-11-10 01:21:40 +0000

    beep

--- main_pi.py
+++ main_pi.py
@@ -17,6 +17,17 @@ joystick.init()
 
 print (joystick.get_name)
 
+def current_check(m_1, m_2, m_3, m_4):
+        value = abs(m_1) + abs(m_2) + abs(m_3) + abs(m_4)
+        if value > 2:
+                e_1 = m_1 * 0.75 ** ((value-2)/2)
+                e_2 = m_2 * 0.75 ** ((value-2)/2)
+                e_3 = m_3 * 0.75 ** ((value-2)/2)
+                e_4 = m_4 * 0.75 ** ((value-2)/2)
+                return e_1,e_2,e_3,e_4
+        else:
+                return m_1, m_2, m_3, m_4
+
 def writeNumber(value):
         value = int(value)
 	bus.write_byte(address, value)
@@ -28,37 +39,55 @@ def readNumber():
 	number = bus.read_byte_data(address, 1)
 	return number
 ##Setup
+err_time = 0
 end = True
 axis_forward_back = 1
 axis_left_right = 0
 axis_up_down = 2
 axis_twist = 3
-t_wait = 0.005
+t_wait = 0.01
+
 while True:
         try:
                 
                 pygame.event.pump()
-                
-                forwLeft = joystick.get_axis(axis_forward_back) * 63 + 64
-                writeNumber(forwLeft)
-                         
-                time.sleep(t_wait)
+                m_1, m_2, m_3, m_4 = current_check(joystick.get_axis(axis_forward_back),joystick.get_axis(axis_left_right),joystick.get_axis(axis_up_down),joystick.get_axis(axis_up_down))
 
-                forwRight = joystick.get_axis(axis_forward_back) * 63 + 64
+                forwLeft = (m_1 + m_2/2) * 63 + 64
+                forwRight = (m_1 - m_2/2) * 63 + 64
+                if (m_1 + m_2/2) > 1:
+                        forwLeft = 1 * 63 + 64
+
+                if (m_1 + m_2/2) < -1:
+                        forwLeft = -1 * 63 + 64
+
+                if (m_1 - m_2/2) > 1:
+                        forwRight = 1 * 63 + 64
+                        
+                if (m_1 - m_2/2) < -1:
+                        forwRight = -1 * 63 + 64
+                
+                vertLeft = m_3 * 63 + 64
+                vertRight = m_4 * 63 + 64
+
+                #print(forwLeft, forwRight, vertLeft, vertRight)
+                
+                writeNumber(forwLeft)
+                time.sleep(t_wait)
+                
                 writeNumber(forwRight)
                 time.sleep(t_wait)
                 
-                vertLeft = joystick.get_axis(axis_up_down) * 63 + 64
                 writeNumber(vertLeft)
                 time.sleep(t_wait)
-		
-                vertRight = joystick.get_axis(axis_up_down) * 63 + 64
+                
                 writeNumber(vertRight)
                 time.sleep(t_wait)
-
+                
                 writeNumber(0)
                 time.sleep(t_wait)
-
+                
+        
                 #print(str(int(forwLeft)) + "\t" + str(int(forwLeft)) + "\t" + str(int(vertLeft)) + "\t" + str(int(vertRight)))
 
                 
@@ -68,15 +97,17 @@ while True:
 
         except IOError as err:
                 end = True
-                time.sleep(0.5)
-                print (err)
+                time.sleep(0.2)
+                err_time +=1
+                #subprocess.call(['i2cdetect', '-y', '1'])
+                print (str(err) + ', ' + str(err_time))
         
 
-        except KeyboardInterrupt: pass
+        except KeyboardInterrupt: break
 
 
 
-
+print ('')
                 
 		
 """
